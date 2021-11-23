18c097790975eb27f9941eb3eaaba4a496bbab14
Surya Krishna Moorthy <37917817+SuryaKrishnaMoorthy@users.noreply.github.com>
2021-11-23 12:27:55 -0500

    update readme

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import './App.css';
+import Modal from './Modal/Modal';
+import Backdrop from './Backdrop/Backdrop';
+import List from './List/List';
 
 function App() {
   return (
     <div className="App">
-      
+      <h1>React Animations</h1>
+      <Modal />
+      <Backdrop />
+      <button className="Button">Open Modal</button>
+      <h3>Animating Lists</h3>
+      <List />
     </div>
   );
 }
